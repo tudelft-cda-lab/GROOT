b72a92571e34e12ada45dadbc0905b9c3c3daf24
Daniel Vos <daniel.vos.d@gmail.com>
2022-03-10 16:03:44 +0100

    Add getting started example code to pytest

--- tests/test_getting_started.py
+++ tests/test_getting_started.py
@@ -3,25 +3,26 @@ from groot.toolbox import Model
 
 from sklearn.datasets import make_moons
 
-# Load the dataset
-X, y = make_moons(noise=0.3, random_state=0)
-X_test, y_test = make_moons(noise=0.3, random_state=1)
+def test_getting_started_example():
+    # Load the dataset
+    X, y = make_moons(noise=0.3, random_state=0)
+    X_test, y_test = make_moons(noise=0.3, random_state=1)
 
-# Define the attacker's capabilities (L-inf norm radius 0.3)
-epsilon = 0.3
-attack_model = [epsilon, epsilon]
+    # Define the attacker's capabilities (L-inf norm radius 0.3)
+    epsilon = 0.3
+    attack_model = [epsilon, epsilon]
 
-# Create and fit a GROOT tree
-tree = GrootTreeClassifier(
-    attack_model=attack_model,
-    random_state=0
-)
-tree.fit(X, y)
+    # Create and fit a GROOT tree
+    tree = GrootTreeClassifier(
+        attack_model=attack_model,
+        random_state=0
+    )
+    tree.fit(X, y)
 
-# Determine the accuracy and accuracy against attackers
-accuracy = tree.score(X_test, y_test)
-model = Model.from_groot(tree)
-adversarial_accuracy = model.adversarial_accuracy(X_test, y_test, attack="tree", epsilon=0.3)
+    # Determine the accuracy and accuracy against attackers
+    accuracy = tree.score(X_test, y_test)
+    model = Model.from_groot(tree)
+    adversarial_accuracy = model.adversarial_accuracy(X_test, y_test, attack="tree", epsilon=0.3)
 
-print("Accuracy:", accuracy)
-print("Adversarial Accuracy:", adversarial_accuracy)
+    print("Accuracy:", accuracy)
+    print("Adversarial Accuracy:", adversarial_accuracy)
